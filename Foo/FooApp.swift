100c8f3ef4c33108b2ce9b330c0b859785dd53de
Hwee-Boon Yar <hboon@motionobj.com>
2024-02-12 11:35:10 +0800

    After add OTHER_LDFLAGS, not add frameworks yet

---
+++ Foo/FooApp.swift
@@ -0,0 +1,17 @@
+//
+//  FooApp.swift
+//  Foo
+//
+//  Created by Hwee-Boon Yar on 12/2/24.
+//
+
+import SwiftUI
+
+@main
+struct FooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
